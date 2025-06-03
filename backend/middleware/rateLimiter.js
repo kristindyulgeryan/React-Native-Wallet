7fc3480e8a292d7885cbb4682408e154bcda29eb
Kristin <kristindyulgeryan@abv.bg>
2025-06-03 16:14:59 +0300

    Created `routes/transactionsRouter.js` to organize transaction endpoints

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -2,6 +2,7 @@ import ratelimit from "../config/upstash.js";
 
 const ratelimiter = async (req, res, next) => {
   try {
+    // Here just kept it simple. In production, use userId or ipAddress as the rate limit key.
     const { success } = await ratelimit.limit("my-rate-limit");
 
     if (!success) {
