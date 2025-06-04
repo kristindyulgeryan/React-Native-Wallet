1b48467ff56a73b0849f716f2c504b3330516495
Kristin <kristindyulgeryan@abv.bg>
2025-06-04 13:07:16 +0300

    Add date formatting utility in lib/utils.js.

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -2,16 +2,5 @@ import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href={"/about"}>About</Link>
-    </View>
-  );
+  return <View></View>;
 }
